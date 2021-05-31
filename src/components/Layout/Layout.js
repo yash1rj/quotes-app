6382c7e5006b7bb444c3260d718ce06bc0205599
Yash Raj <yashrj130@gmail.com>
2021-05-31 12:34:42 +0530

    Create Layout.js
    -layout of whole app
    -contains navigation bar, sidebar and main app

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,31 @@
+// import classes from './Layout.module.css';
+import React, { Fragment, useState } from 'react';
+import NavBar from './NavBar/NavBar';
+import SideDrawer from './SideDrawer/SideDrawer';
+
+const Layout = props => {
+
+    const [showSideDrawer, setShowSideDrawer] = useState(false);
+
+    const sideDrawerClosedHandler = () => {
+        setShowSideDrawer(false);
+    }
+
+    const sideDrawerToggleHandler = () => {
+        setShowSideDrawer(true);
+    }
+
+    return (
+        <Fragment>
+            <NavBar drawerToggleClicked={sideDrawerToggleHandler} />
+            <SideDrawer
+                    open={showSideDrawer} 
+                    closed={sideDrawerClosedHandler} />
+            <main style={{marginTop: "64px"}}>
+                {props.children}
+            </main>
+        </Fragment>
+    );
+};
+
+export default Layout;

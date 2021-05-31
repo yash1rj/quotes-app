8a92bf934cda5f10d88680c854aa1be7b897681a
Yash Raj <yashrj130@gmail.com>
2021-05-31 12:35:42 +0530

    Side drawer for mobile devices

---
+++ src/components/Layout/SideDrawer/SideDrawer.js
@@ -0,0 +1,38 @@
+import classes from './SideDrawer.module.css';
+import React, { Fragment } from 'react';
+import { NavLink } from 'react-router-dom';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+
+const SideDrawer = props => {
+
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
+    return (
+        <Fragment>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={attachedClasses.join(' ')} onClick={props.closed}>
+                <nav>
+                    <ul className={classes.NavigationItems}>
+                        <li className={classes.NavigationItem}>
+                            <NavLink
+                                to="/"
+                                exact
+                                activeClassName={classes.active}>All Quotes</NavLink>
+                        </li>
+                        <li className={classes.NavigationItem}>
+                            <NavLink
+                                to="/new-quote"
+                                exact
+                                activeClassName={classes.active}>Add a Quote</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </Fragment>
+    );
+};
+
+export default SideDrawer;

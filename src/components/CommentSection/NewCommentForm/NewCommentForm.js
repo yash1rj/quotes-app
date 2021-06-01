040dbe0eec040458f3caaf680cc77f0d31a640c1
Yash Raj <yashrj130@gmail.com>
2021-06-02 00:16:31 +0530

    New comment add form component
    also styling

---
+++ src/components/CommentSection/NewCommentForm/NewCommentForm.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef } from 'react';
+import useHttp from '../../../hooks/useHttp';
+import Card from '../../UI/Card/Card';
+import classes from './NewCommentForm.module.css';
+import { addComment } from '../../../lib/api';
+import Spinner from '../../UI/Spinner/Spinner';
+
+const NewCommentForm = props => {
+
+    const { sendRequest, error, status } = useHttp(addComment);
+
+    const commentRef = useRef();
+
+    const { onAddedComment } = props;
+
+    useEffect(() => {
+        if(status === 'completed' && !error) {
+            onAddedComment();
+        }
+    }, [status, error, onAddedComment]);
+
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+
+        // console.log(commentRef.current.value);
+        sendRequest({ commentData: {text: commentRef.current.value}, quoteId: props.qId});
+    };
+
+    return (
+        <Card>
+            <form className={classes.form} onSubmit={formSubmitHandler}>
+                {status === 'pending' && (<div className={classes.loading}>
+                    <Spinner />
+                </div>)}
+                <div className={classes.control}>
+                    <label htmlFor='comment'>Your Comment</label>
+                    <textarea id='comment' rows='4' ref={commentRef} type='text'></textarea>
+                </div>
+                <div className={classes.actions}>
+                    <button className='btn'>Add Comment</button>
+                </div>
+            </form>
+        </Card>
+    );
+};
+
+export default NewCommentForm;
